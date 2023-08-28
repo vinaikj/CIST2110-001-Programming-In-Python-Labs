891059e776958a4206f73baee5c82d66a106b1bc
Greg Walsh <walshg3@go.stockton.edu>
2023-08-27 23:55:30 -0400

    added Lab1

---
+++ Lesson1/Lab1.py
@@ -0,0 +1,10 @@
+# Lab1 
+# Author:
+
+
+# 1. Uncomment the following print statement and run the program. Show the output to your instructor.
+# Note: to "uncomment a line", remove the # at the beginning of the line.
+# Note2: to run the program, Select the "Play" button at the top of the screen.
+
+#print("Hello World!")
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# CIST2110-001-Programming-In-Python-Labs-Instructor
+# CIST2110-001-Programming-In-Python-Labs
+
+This repository contains all of the lab assignments for CIST 2110-001 Programming in Python at Stockton University.
