cf9c42b421aad5214d7eb33fc745038bed5b4dfb
walshg3@go.stockton.edu <walshg3@go.stockton.edu>
2023-09-06 01:09:29 -0400

    lab1 update

--- Lesson1/Lab1.py
+++ Lesson1/Lab1.py
@@ -3,8 +3,12 @@
 
 
 # 1. Uncomment the following print statement and run the program. Show the output to your instructor.
-# Note: to "uncomment a line", remove the # at the beginning of the line.
+# Note: to "uncomment a line", remove the # at the beginning of the line. The shortcut is to select the line and press Ctrl + / (windows) or Command + / (mac)
 # Note2: to run the program, Select the "Play" button at the top of the screen.
 
 #print("Hello World!")
+
+# 2. Add a print statement that prints your name. Run the program and show the output to your instructor.
+
+# 3. Add a print statement that prints your favorite color. Run the program and show the output to your instructor.
 
