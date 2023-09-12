f6a0d99e57b42ccd7dd824a4c471b6905339c09b
walshg3 <walshg3@go.stockton.edu>
2023-09-12 19:21:31 -0400

    completed Lab1

--- Lesson1/Lab1.py
+++ Lesson1/Lab1.py
@@ -1,4 +1,4 @@
-# Lab1 
+# Lab1
 # Author: Greg Walsh
 
 
@@ -6,9 +6,12 @@
 # Note: to "uncomment a line", remove the # at the beginning of the line. The shortcut is to select the line and press Ctrl + / (windows) or Command + / (mac)
 # Note2: to run the program, Select the "Play" button at the top of the screen.
 
-#print("Hello World!")
+print("Hello World!")
 
 # 2. Add a print statement that prints your name. Run the program and show the output to your instructor.
 
+print("Greg Walsh")
+
 # 3. Add a print statement that prints your favorite color. Run the program and show the output to your instructor.
 
+print("Black")
