aa62f1c6e9cfda32dd1d8eefe97e6292de4db182
walshg3 <walshg3@go.stockton.edu>
2023-09-18 23:33:02 -0400

    added lab2 and lab3

---
+++ Week3/Lab2.py
@@ -0,0 +1,54 @@
+# Lab2
+# Author: 
+
+"""_summary_
+This lab is designed to get you familiar with Python Data Types, Variables, and Arithmetic Operators.
+"""
+    
+# 1. Create a variable called "name" and assign it the value of your name. Print the variable.
+
+# 2. Create a variable called "age" and assign it the value of your age. Print the variable.
+
+# 3. Create a variable called "favorite_color" and assign it the value of your favorite color. Print the variable.
+
+# 4. Create a variable called "favorite_movie" and assign it the value of your favorite movie. Print the variable.
+
+# 5. Use string concatenation to print the following sentence: "Hello, my name is <name>. I am <age> years old. My favorite color is <favorite_color> and my favorite movie is <favorite_movie>."
+# Note: punctuation and capitalization matters. Make sure your sentence matches the one above exactly.
+
+# 6. Create a variable called "my_age_in_dog_years" and assign it the value of your age multiplied by 7. 
+
+# 7. Create a variable called "days_in_a_year" and assign it the value of 365.
+
+# 8. Create a variable called "days_alive" and assign it the value of your age multiplied by the number of days in a year.
+
+# 9. Create a variable called "days_alive_in_dog_years" and assign it the value of your age in dog years multiplied by the number of days in a year.
+
+# 10. Create a chart that shows the following information:
+# - Your name
+# - Your age
+# - Your favorite color
+# - Your favorite movie
+# - Your age in dog years
+# - Number of days in a year
+# - Number of days you have been alive
+# - Number of days you have been alive in dog years
+
+
+"""
+Make it look pretty as well as informative. Use the following format:
+==================================
+| Name: John Doe                 |
+| Age: 25                        |
+| Favorite Color: Blue           |
+| Favorite Movie: The Matrix     |
+| Age in Dog Years: 175          |
+| Days in a Year: 365            |
+| Days Alive: 9125               |
+| Days Alive in Dog Years: 63875 |
+==================================
+Hints:
+- Use the string concatenation operator (+) to combine strings and variables
+- print has an optional parameter called "end" that can be used to change the default newline character (\n) to something else or maybe even add a tab (\t) to the end of the line (or multiple tabs)
+- You will need to use conversion functions to convert your numbers to strings
+"""
---
+++ Week3/Lab3.py
@@ -0,0 +1,26 @@
+# Lab3
+# Author: 
+
+"""_summary_
+This lab is designed to get you familiar with Python input(), converting data types, and using the round() function.
+"""
+
+# 1. Ask the user for their name and assign it to a variable called "name".
+
+# 2. Ask the user for their age and assign it to a variable called "age".
+
+# 3. Ask the user for a balance and assign it to a variable called "balance".
+
+# 4. Ask the user for a number of years to calculate interest and assign it to a variable called "years".
+
+# 5. Ask the user for an interest rate and assign it to a variable called "interest_rate".
+
+# 6. Convert the balance, years, and interest_rate to float data types.
+# Note: This is only necessary if you did not specify the data type in the input() function.
+
+# 7. Calculate the future balance using the formula: balance * (1 + interest_rate/100) ** years
+
+# 8. Round the future balance to 2 decimal places using the round() function.
+
+# 9. Print the following sentence using string concatenation: "Hello <name>, your balance after <years> years will be $<future_balance>."
+
