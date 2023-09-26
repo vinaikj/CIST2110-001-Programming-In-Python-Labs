070fa69be294418ad14693b981fb667a588916ee
walshg3 <walshg3@go.stockton.edu>
2023-09-26 01:37:14 -0400

    Added Lab4

---
+++ Week4/Lab4.py
@@ -0,0 +1,22 @@
+# Lab3
+# Author: 
+
+"""_summary_
+This lab is designed to get you familiar with Python Boolean Expressions, Conditional Expressions, if-elif-else statements, and while / for loops.
+"""
+
+# 1. Write some code that asks the user for a number and prints out whether it is positive, negative, or zero.
+
+
+# 2. Write some code that asks the user for a number and prints out whether it is even or odd.
+
+
+# 3. Write some code that asks the user for a number and prints out all the numbers from 1 to that number using a while loop.
+
+# 4. Use a for loop to ask a user for 5 numbers and print out the average of those 5 numbers.
+
+# 5. Write some code that prints out all the numbers from 1 to 10 that are divisible by 3 or 5.
+
+# 6. Write some code that asks the user for a number and then prints out a countdown from that number to 1 using a while loop. After the countdown, print "Blast off!".
+
+# 7. Write some code that asks the user for a number and then uses a for loop to iterate from 1 to that number. If the current number is divisible by 7, print "Lucky" and continue to the next iteration. If the current number is greater than 100, break the loop.
