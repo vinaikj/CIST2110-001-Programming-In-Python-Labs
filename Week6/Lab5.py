2ae9e764ec198a22c2bec221de3b18862af4b281
walshg3 <walshg3@go.stockton.edu>
2023-10-10 17:51:25 -0400

    added lab5

---
+++ Week6/Lab5.py
@@ -0,0 +1,25 @@
+# Lab4
+# Author: 
+
+"""_summary_
+This lab is designed to get you familiar with Python virtual environments as well as import statments to use external libraries.
+"""
+
+# 1. Create a virtual environment called "venv" in the current directory. (Type command here in comments)
+
+# 2. Activate the virtual environment. (Type command here in comments)
+
+# 3. Install the requests library. (Type command here in comments)
+
+# 4. import requests library here
+
+# 5. Use the requests library to make a GET request to https://api.github.com/events
+
+# 6. Print out the status code of the response.
+
+# 7. Print out the content of the response.
+
+# 8. Print out the headers of the response.
+
+# 9. Deactivate the virtual environment. (Type command here in comments)
+
